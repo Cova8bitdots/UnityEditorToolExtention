f5f741b7d27040b11cccb561b10ca86f3ce56e9c
Cova8bitdot <cova.tech.8bitdots@gmail.com>
2020-06-04 12:43:00 +0900

    自動再描画を行うEditor基底クラスを追加

---
+++ Editor/EditorWindowBase.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace cova.EditorTool
+{
+    /// <summary>
+    /// EditorWindow の基本クラス
+    /// (自動更新をするように)
+    /// </summary>
+    public class EditorWindowBase : EditorWindow
+    {
+        //-----------------------------------------------------
+        // GUI 自動再描画
+        //-----------------------------------------------------
+        #region ===== AUTO_REPAINT =====
+
+
+        /// <summary>
+        /// EditorApplication.update Eventに登録
+        /// </summary>
+        private void OnEditorUpdate()
+        {
+            Repaint();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected virtual void OnEnable()
+        { 
+            EditorApplication.update += OnEditorUpdate;
+        }
+        protected virtual void OnDisable()
+        {
+            EditorApplication.update -= OnEditorUpdate;
+        }
+        #endregion //) ===== AUTO_REPAINT =====
+
+    }
+}
