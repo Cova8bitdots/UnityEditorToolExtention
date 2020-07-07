e510295308aa0fb7a52a2fdd98e81e4995798802
Cova8bitdot <cova.tech.8bitdots@gmail.com>
2020-07-07 17:50:05 +0900

    [EditorTools] EnumLabel属性追加

---
+++ Tools/Attribute/Editor/EnumLabelAttributeEditor.cs
@@ -0,0 +1,41 @@
+﻿/**
+EnumLabel.cs
+Copyright (c) 2016 Hassaku
+This software is released under the MIT License.
+http://opensource.org/licenses/mit-license.php
+https://gist.github.com/HassakuTb/30dceff6fcfeca73fa5ea6a5ecbbc08f
+*/
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEditor;
+
+namespace CovaTech.EditorTools
+{
+    [CustomPropertyDrawer(typeof(EnumLabelAttribute))]
+    public class EnumLabelDrawer : PropertyDrawer {
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+
+            EnumLabelAttribute attr = attribute as EnumLabelAttribute;
+            Match match = new Regex(@"Element (?<index>\d+)").Match(label.text);
+            if(!match.Success){
+                EditorGUI.PropertyField(position, property, label);
+                return;
+            }
+            int index = int.Parse(match.Groups["index"].Value);
+
+            if (index < attr.EnumNames.Length) {
+                EditorGUI.PropertyField(position, property, new GUIContent(attr.EnumNames[index]), true);
+            }
+            else {
+                EditorGUI.PropertyField(position, property, label, true);
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+            return EditorGUI.GetPropertyHeight(property);
+        }
+    }
+}
---
+++ Tools/Attribute/EnumLabelAttribute.cs
@@ -0,0 +1,25 @@
+﻿
+/**
+EnumLabel.cs
+Copyright (c) 2016 Hassaku
+This software is released under the MIT License.
+http://opensource.org/licenses/mit-license.php
+https://gist.github.com/HassakuTb/30dceff6fcfeca73fa5ea6a5ecbbc08f
+*/
+using UnityEngine;
+using UnityEngine.Assertions;
+using System;
+
+namespace CovaTech.EditorTools
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class EnumLabelAttribute : PropertyAttribute{
+
+        public string[] EnumNames { get; private set; }
+
+        public EnumLabelAttribute(Type enumType) {
+            Assert.IsTrue(enumType.IsEnum, "[EnumLabel] type of attribute parameter is not enum.");
+            EnumNames = Enum.GetNames(enumType);
+        }
+    }
+}
