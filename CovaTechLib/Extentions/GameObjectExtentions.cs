199d9d46f989b32b0c955a0b9ac0b939fb48afd3
Cova8bitdot <cova.tech.8bitdots@gmail.com>
2020-08-05 16:25:48 +0900

    [Extention] GameObjectExtention 追加

---
+++ CovaTechLib/Extentions/GameObjectExtentions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace CovaTech.Extentions
+{
+    /// <summary>
+    /// GameObjectの拡張
+    ///</summary>
+    public static class GameObjectExtentions
+    {
+        /// <summary>
+        /// SetActive の改良版
+        /// 不必要にSetActiveを呼ばないように拡張
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="isActive"></param>
+        public static void SetActiveIfRequired(this GameObject self, bool isActive)
+        {
+            if (self == null || self.activeSelf == isActive)
+            {
+                return;
+            }
+            self.SetActive(isActive);
+        }
+
+    }
+}
